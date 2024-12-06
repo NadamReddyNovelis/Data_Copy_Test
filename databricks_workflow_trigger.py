80ffc09950b1ced496f7d788c293552864458615
NadamReddyNovelis <166607224+NadamReddyNovelis@users.noreply.github.com>
2024-12-06 11:16:18 +0530

    Create databricks_workflow_trigger.py
    Databricks-sdk : TO trigger DBR workflows using ext python script

---
+++ databricks_workflow_trigger.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import pytz
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service import jobs
+
+def trigger_job_run_sdk(job_id, params):
+    try:
+        run = workspace_client.jobs.run_now(
+            job_id=job_id,
+            notebook_params=params
+        )
+        print(f'Triggered a job run with run_id: {run.response.run_id}')
+        
+        run_res = run.result().as_dict()
+        return {key: run_result.get(key) for key in ['run_id', 'run_page_url', 'state']}
+    except Exception as e:
+        raise Exception(f'Failed to trigger workflow run: {e}')
+
+if __name__ == '__main__':
+    base_uri = f'https://adb-{workspace_id}.azuredatabricks.net/'
+    workspace_client = WorkspaceClient(
+        host=base_uri,
+        token=api_token,
+    )
+
+    run_result = trigger_job_run_sdk(job_id=job_id, params=params)
+    
+    print(f'Job at: {run_result["run_page_url"]} completed with result state: {run_result["state"]}')
+    if run_result['state'] in ('SUCCESSFUL'):
+        pass
+    else:
+        raise Exception('Job run failed.')
+        
