182ebb59f27e93a51ed380d8f1ce4c00c0181f4b
NadamReddyNovelis <166607224+NadamReddyNovelis@users.noreply.github.com>
2024-12-09 19:36:41 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 import json
 import sys
 print('__________________________________________')
-print(sys.argv[1])
+# print(sys.argv[1])
 
-inputs = json.loads(sys.argv[1])
+# inputs = json.loads(sys.argv[1])
 
-for key, value in inputs.items():
-    print(f"{key}: {value}")
+# for key, value in inputs.items():
+#     print(f"{key}: {value}")
