6009fcb29a51f4d62a11a0fff0a9d8ae401fe053
NadamReddyNovelis <166607224+NadamReddyNovelis@users.noreply.github.com>
2024-12-09 19:51:17 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,18 +3,21 @@ import sys
 
 # Print debugging information
 print('__________________________________________')
-print(f"Argument received: {sys.argv[1]}")
-
-# try:
-#     for key, value in inputs.items():
-#         print(f"{key}: {value}")
-
-# except FileNotFoundError:
-#     print("Error: File not found.")
-#     sys.exit(1)
-# except json.JSONDecodeError as e:
-#     print(f"Error: Invalid JSON format: {e}")
-#     sys.exit(1)
-# except Exception as e:
-#     print(f"Unexpected error: {e}")
-#     sys.exit(1)
+input_args = sys.argv[1]
+print(f"Argument received: {input_args}")
+print("Type : ",type(input_args))
+try:
+    if type(input_args) == str:
+        inputs = json.loads(input_args)
+    elif type(input_args) == dict:
+        pass
+    else:
+        sys.exit(1)
+    for key, value in inputs.items():
+        print(f"{key}: {value}")
+except json.JSONDecodeError as e:
+    print(f"Error: Invalid JSON format: {e}")
+    sys.exit(1)
+except Exception as e:
+    print(f"Unexpected error: {e}")
+    sys.exit(1)
